5beb16332d017145af0c1e0c3e326f5ff4fe568f
Dz-devz <darwinbjordan@gmail.com>
2024-10-28 19:24:32 +0800

    chore: added text effect

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -1,5 +1,4 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
-import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 
@@ -8,24 +7,8 @@ export const Route = createLazyFileRoute('/about')({
 })
 
 export function About() {
-  const [displayedText, setDisplayedText] = useState('');
-  const fullText = "I'm a Passionate Full Stack Developer dedicated to honing my skills through continual growth and improvement With a head full of Profound ideas, I'm on a journey to turn concept and ideas into reality through code." +
-        " Outside of coding, I create music to nurture my artistic side and prevent burnout.";
+  const fullText = "I'm a Passionate Full Stack Developer dedicated to honing my skills through continual growth and improvement With a head full of Profound ideas, I'm on a journey to turn concept and ideas into reality through code."
   
-  useEffect(() => {
-    let currentIndex = 0;
-
-    const typingInterval = setInterval(() => {
-      setDisplayedText((prevText) => prevText + fullText[currentIndex]);
-      currentIndex++;
-
-      if (currentIndex === fullText.length) {
-        clearInterval(typingInterval);
-      }
-    }, 100); // Adjust typing speed (milliseconds)
-
-    return () => clearInterval(typingInterval);
-  }, [fullText]);
 
   return (
     <div className='flex item-center justify-center h-screen'>
@@ -35,7 +18,7 @@ export function About() {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
     >
-      <span>{displayedText}</span>
+      <span>{fullText}</span>
     </motion.div>
       </div>
     </div>

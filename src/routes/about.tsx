10a4f2cfc361df081cfb28f6362178b26820b296
Dz-devz <darwinbjordan@gmail.com>
2024-10-29 20:24:23 +0800

    chore: change lazy to just normal routes

---
+++ src/routes/about.tsx
@@ -0,0 +1,25 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { motion } from 'framer-motion'
+
+export const Route = createFileRoute('/about')({
+  component: About,
+})
+
+export function About() {
+  const fullText =
+    "I'm a Passionate Full Stack Developer dedicated to honing my skills through continual growth and improvement With a head full of Profound ideas, I'm on a journey to turn concept and ideas into reality through code."
+
+  return (
+    <div className="flex item-center justify-center h-screen">
+      <div className="text-left text-2xl w-[800px]">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <span>{fullText}</span>
+        </motion.div>
+      </div>
+    </div>
+  )
+}

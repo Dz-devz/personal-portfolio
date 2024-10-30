87b9b5e70fe63029705f19e4074f551e4ab05738
Dz.dev <darwinbjordan@gmail.com>
2024-10-30 22:50:49 +0800

    chore: imporve contact details and ui

--- src/routes/projects.tsx
+++ src/routes/projects.tsx
@@ -5,6 +5,7 @@ import pokemonthumbnail from "@/assets/pokemonthumbnail.png";
 import prisma from "@/assets/prisma.png";
 import blogthumbnail from "@/assets/thumbnail.png";
 import typescript from "@/assets/typescript.png";
+import BlurFade from "@/components/ui/blur-fade";
 import { BorderBeam } from "@/components/ui/border-beam";
 import { Card, CardDescription, CardTitle } from "@/components/ui/card";
 import {
@@ -13,7 +14,6 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import BlurFade from "@/components/ui/blur-fade";
 import { createFileRoute, Link } from "@tanstack/react-router";
 
 const thumbnails = [
@@ -52,65 +52,76 @@ export const Route = createFileRoute("/projects")({
 export default function Projects() {
   return (
     <>
-    <BlurFade delay={0.25} inView>
-      <div className="text-2xl text-center font-chivo mb-2 font-bold">Apps I’ve Crafted</div>
-    </BlurFade>
-    <BlurFade delay={0.50} inView>
-      <div className="text-2xl text-center font-chivo text-[#57AD5B] mb-2">Projects</div>
-    </BlurFade>
-    <BlurFade delay={0.75} inView>
-      <h1 className="text-2xl text-center font-chivo mt-2 w-[800px] mr-auto ml-auto">Take a look at some of my recent projects—practical <br/> solutions built to refine my skills and bring useful features to life.</h1>
-    </BlurFade>
+      <BlurFade delay={0.25} inView>
+        <div className="text-2xl text-center font-chivo text-[#57AD5B] mb-2">
+          Projects
+        </div>
+      </BlurFade>
+      <BlurFade delay={0.5} inView>
+        <div className="text-4xl text-center font-chivo mb-2 font-bold">
+          Apps I’ve Crafted
+        </div>
+      </BlurFade>
+      <BlurFade delay={0.75} inView>
+        <h1 className="text-2xl text-center font-chivo mt-2 w-[800px] mr-auto ml-auto">
+          Take a look at some of my recent projects—practical <br /> solutions
+          built to refine my skills and bring useful features to life.
+        </h1>
+      </BlurFade>
       <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-12 flex gap-4">
         {thumbnails.map((thumbnail, idx) => (
-      <BlurFade key={thumbnail.id} delay={1 + idx * 0.5} inView>
-          <TooltipProvider>
-            <Card className="relative mb-4 w-96 h-40 flex items-center justify-center">
-              <BorderBeam
-                size={200}
-                duration={12 * thumbnail.id}
-                delay={9 * thumbnail.id}
-                colorFrom="#000000"
-                colorTo="#57AD5B"
-              />
-              <Tooltip>
-                <TooltipTrigger>
-                <Link to={thumbnail.link} target="_blank" rel="noopener noreferrer">
-                  <div className="relative font-chivo">
-                      <div className="flex flex-col">
-                        <CardTitle className="text-xl md:text-2xl lg:text-3xl text-[#57AD5B]">
-                          {thumbnail.title}
-                        </CardTitle>
-                        <CardDescription className="text-muted-foreground text-sm md:text-base lg:text-lg">
-                          {thumbnail.desc}
-                        </CardDescription>
-                        <div className="flex gap-2">
-                          {Object.entries(thumbnail.tools).map(
-                            ([toolName, toolIcon]) => (
-                              <img
-                                key={toolName}
-                                src={toolIcon}
-                                alt={`${toolName} icon`}
-                                className="w-6 h-6"
+          <BlurFade key={thumbnail.id} delay={1 + idx * 0.5} inView>
+            <TooltipProvider>
+              <Card className="relative mb-4 w-96 h-40 flex items-center justify-center">
+                <BorderBeam
+                  size={200}
+                  duration={12 * thumbnail.id}
+                  delay={9 * thumbnail.id}
+                  colorFrom="#000000"
+                  colorTo="#57AD5B"
+                />
+                <Tooltip>
+                  <TooltipTrigger>
+                    <Link
+                      to={thumbnail.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <div className="relative font-chivo">
+                        <div className="flex flex-col">
+                          <CardTitle className="text-xl md:text-2xl lg:text-3xl text-[#57AD5B]">
+                            {thumbnail.title}
+                          </CardTitle>
+                          <CardDescription className="text-muted-foreground text-sm md:text-base lg:text-lg">
+                            {thumbnail.desc}
+                          </CardDescription>
+                          <div className="flex gap-2">
+                            {Object.entries(thumbnail.tools).map(
+                              ([toolName, toolIcon]) => (
+                                <img
+                                  key={toolName}
+                                  src={toolIcon}
+                                  alt={`${toolName} icon`}
+                                  className="w-6 h-6"
                                 />
                               )
                             )}
+                          </div>
                         </div>
                       </div>
-                  </div>
-                </Link>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <img
-                    src={thumbnail.thumbnail}
-                    alt="Thumbnail"
-                    className="w-[500px] h-[350px] rounded shadow-lg transition-transform transform group-hover:scale-105"
-                  />
-                </TooltipContent>
-              </Tooltip>
-            </Card>
-          </TooltipProvider>
-        </BlurFade>
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <img
+                      src={thumbnail.thumbnail}
+                      alt="Thumbnail"
+                      className="w-[500px] h-[350px] rounded shadow-lg transition-transform transform group-hover:scale-105"
+                    />
+                  </TooltipContent>
+                </Tooltip>
+              </Card>
+            </TooltipProvider>
+          </BlurFade>
         ))}
       </div>
     </>

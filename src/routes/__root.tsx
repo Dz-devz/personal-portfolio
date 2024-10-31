bdcf5f233cfea9eb50841d6db06ea02413b1515e
Dz.dev <darwinbjordan@gmail.com>
2024-10-31 16:29:37 +0800

    chore: hamburger menu

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,6 +9,7 @@ export const Route = createRootRoute({
 
 function Root() {
   const [rootShow, setRootShow] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     // Check local storage to see if the root has already been shown
@@ -31,6 +32,10 @@ function Root() {
     visible: { opacity: 1, transition: { delay: 1, duration: 1 } },
   };
 
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev); // Toggle the menu open state
+  };
+
   return (
     <>
       <div className="p-4 text-lg flex flex-col md:flex-row justify-between items-center bg-[#ECE8E1] font-chivo">
@@ -45,12 +50,31 @@ function Root() {
             <p className="hidden md:block">dz.dev</p>{" "}
           </motion.div>
         )}
+        <button
+          className="md:hidden flex items-center justify-center p-2"
+          onClick={toggleMenu}
+          aria-label="Toggle menu"
+        >
+          <div className="flex gap-[4px] flex-col">
+            <div
+              className={`w-6 h-0.5 mb-1 bg-black transition-all duration-300 ${menuOpen ? "rotate-45 translate-y-2" : ""}`}
+            ></div>
+            <div
+              className={`w-6 h-0.5 bg-black transition-all duration-300 ${menuOpen ? "opacity-0" : "mb-1"}`}
+            ></div>
+            <div
+              className={`w-6 h-0.5 bg-black transition-all duration-300 ${menuOpen ? "-rotate-45 -translate-y-2" : ""}`}
+            ></div>
+          </div>
+        </button>
         {rootShow && (
           <motion.div
             initial="hidden"
             animate="visible"
             variants={pageVariants}
-            className="flex flex-col md:flex-row gap-4 lg:gap-[98px] md:[50px] mr-4 mt-4"
+            className={`flex flex-col md:flex-row gap-4 lg:gap-[98px] md:[50px] mr-4 mt-4 ${
+              menuOpen ? "block" : "hidden md:flex"
+            }`}
           >
             <Link to="/" className="hover:underline [&.active]:font-bold">
               Home

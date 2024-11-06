bd6305946eb20699452eb204057e701e120ef5c1
Dz-devz <darwinbjordan@gmail.com>
2024-11-06 19:37:47 +0800

    chore: responsive home page image and get rid of white space on bottom

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -11,13 +11,13 @@ export default function Home() {
 
           <img
             src={background}
-            className="w-full max-w-[1000px] h-auto rounded-xl border-4 border-[#ECE8E1] shadow-lg transform rotate-3"
+            className="xl:max-w-[900px] lg:max-w-[700px] md:max-w-[600px] sm:max-w-[300px] h-auto rounded-xl border-4 border-[#ECE8E1] shadow-lg transform rotate-3"
             alt="Profile background"
           />
-          <div className="absolute md:bottom-20 bottom-2 right-6 text-center left-10">
+          <div className="absolute md:bottom-16 bottom-2 right-6 text-center left-10">
             <p className="font-chivo text-white font-semibold">
               <TypingAnimation
-                className="text-md lg:text-4xl md:text-2xl"
+                className="text-md lg:text-3xl md:text-2xl"
                 text="I'm dz.dev Discover projects, skills, and new ideas as I share my
               journey as a passionate Full Stack Developer. Let's build
               something amazing together!"

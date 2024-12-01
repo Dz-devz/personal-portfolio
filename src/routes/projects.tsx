89e40cdad49140dc73381bd9c6aa7b6229ed9b1d
Dz.dev <darwinbjordan@gmail.com>
2024-12-01 15:38:02 +0800

    chore: added a section for client portfolio

--- src/routes/projects.tsx
+++ src/routes/projects.tsx
@@ -1,3 +1,4 @@
+import bellethumbnail from "@/assets/bellethumbnail.png";
 import drizzlekit from "@/assets/drizzlekit.png";
 import honojs from "@/assets/honojs.png";
 import javascript from "@/assets/javascript.png";
@@ -40,6 +41,7 @@ const thumbnails = [
       node: nodejs,
       hono: honojs,
     },
+    category: "Personal",
   },
   {
     id: 2,
@@ -54,6 +56,7 @@ const thumbnails = [
       prisma: prisma,
       tailwind: tailwind,
     },
+    category: "Personal",
   },
   {
     id: 3,
@@ -68,6 +71,7 @@ const thumbnails = [
       tailwind: tailwind,
       pokemon: pokemon,
     },
+    category: "Personal",
   },
   {
     id: 4,
@@ -81,6 +85,21 @@ const thumbnails = [
       reactjs: reactjs,
       tailwind: tailwind,
     },
+    category: "Personal",
+  },
+  {
+    id: 5,
+    title: "Belle Website",
+    desc: "Personal Portfolio",
+    thumbnail: bellethumbnail,
+    link: "https://belle-website.vercel.app/",
+    tools: {
+      javascript: javascript,
+      typescript: typescript,
+      reactjs: reactjs,
+      tailwind: tailwind,
+    },
+    category: "Client",
   },
 ];
 
@@ -108,63 +127,137 @@ export default function Projects() {
         </h1>
       </BlurFade> */}
       <div className="mx-auto max-w-screen-xl flex-col lg:flex-row flex-wrap px-4 py-8 lg:px-12 xl:justify-normal lg:justify-center flex gap-4">
-        {thumbnails.map((thumbnail, idx) => (
-          <BlurFade key={thumbnail.id} delay={1 + idx * 0.5} inView>
-            <TooltipProvider>
-              <Card className="relative mb-4 p-4 lg:w-96 lg:h-36 md:w-96 md:h-36 sm:w-40 sm:h-36 flex items-center justify-center">
-                <BorderBeam
-                  size={80}
-                  duration={12 * thumbnail.id}
-                  delay={9 * thumbnail.id}
-                  colorFrom="#000000"
-                  colorTo="#57AD5B"
-                />
-                <Tooltip>
-                  <TooltipTrigger>
-                    <Link
-                      to={thumbnail.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <div className="relative font-chivo">
-                        <div className="flex flex-col">
-                          <CardTitle className="text-xl md:text-2xl lg:text-3xl text-[#57AD5B]">
-                            {thumbnail.title}
-                          </CardTitle>
-                          <CardDescription className="text-muted-foreground text-sm md:text-base lg:text-lg">
-                            {thumbnail.desc}
-                          </CardDescription>
-                          <div className="flex gap-2">
-                            {Object.entries(thumbnail.tools).map(
-                              ([toolName, toolIcon]) => (
-                                <img
-                                  key={toolName}
-                                  src={toolIcon}
-                                  alt={`${toolName} icon`}
-                                  className="w-6 h-6"
-                                />
-                              )
-                            )}
+        {thumbnails
+          .filter((thumbnail) => thumbnail.category === "Personal")
+          .map((thumbnail, idx) => (
+            <BlurFade key={thumbnail.id} delay={1 + idx * 0.5} inView>
+              <TooltipProvider>
+                <Card className="relative mb-4 p-4 lg:w-96 lg:h-36 md:w-96 md:h-36 sm:w-40 sm:h-36 flex items-center justify-center">
+                  <BorderBeam
+                    size={80}
+                    duration={12 * thumbnail.id}
+                    delay={9 * thumbnail.id}
+                    colorFrom="#000000"
+                    colorTo="#57AD5B"
+                  />
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <Link
+                        to={thumbnail.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <div className="relative font-chivo">
+                          <div className="flex flex-col">
+                            <CardTitle className="text-xl md:text-2xl lg:text-3xl text-[#57AD5B]">
+                              {thumbnail.title}
+                            </CardTitle>
+                            <CardDescription className="text-muted-foreground text-sm md:text-base lg:text-lg">
+                              {thumbnail.desc}
+                            </CardDescription>
+                            <div className="flex gap-2">
+                              {Object.entries(thumbnail.tools).map(
+                                ([toolName, toolIcon]) => (
+                                  <img
+                                    key={toolName}
+                                    src={toolIcon}
+                                    alt={`${toolName} icon`}
+                                    className="w-6 h-6"
+                                  />
+                                )
+                              )}
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </Link>
-                  </TooltipTrigger>
-                  {createPortal(
-                    <TooltipContent className="z-[9999]">
-                      <img
-                        src={thumbnail.thumbnail}
-                        alt="Thumbnail"
-                        className="w-[500px] h-[350px] rounded shadow-lg"
-                      />
-                    </TooltipContent>,
-                    document.body
-                  )}
-                </Tooltip>
-              </Card>
-            </TooltipProvider>
-          </BlurFade>
-        ))}
+                      </Link>
+                    </TooltipTrigger>
+                    {createPortal(
+                      <TooltipContent className="z-[9999]">
+                        <img
+                          src={thumbnail.thumbnail}
+                          alt="Thumbnail"
+                          className="w-[500px] h-[350px] rounded shadow-lg"
+                        />
+                      </TooltipContent>,
+                      document.body
+                    )}
+                  </Tooltip>
+                </Card>
+              </TooltipProvider>
+            </BlurFade>
+          ))}
+      </div>
+      <BlurFade delay={0.5} inView>
+        <div className="text-4xl text-center font-chivo mb-2 font-bold text-[#57AD5B]">
+          Apps I’ve Crafted For Client
+        </div>
+      </BlurFade>
+      {/* <BlurFade delay={0.75} inView>
+        <h1 className="text-2xl text-center font-chivo mt-2 max-w-3xl mx-auto">
+          Take a look at some of my recent projects—practical <br /> solutions
+          built to refine my skills and bring useful features to life.
+        </h1>
+      </BlurFade> */}
+      <div className="mx-auto max-w-screen-xl flex-col lg:flex-row flex-wrap px-4 py-8 lg:px-12 xl:justify-normal lg:justify-center flex gap-4">
+        {thumbnails
+          .filter((thumbnail) => thumbnail.category === "Client")
+          .map((thumbnail, idx) => (
+            <BlurFade key={thumbnail.id} delay={1 + idx * 0.5} inView>
+              <TooltipProvider>
+                <Card className="relative mb-4 p-4 lg:w-96 lg:h-36 md:w-96 md:h-36 sm:w-40 sm:h-36 flex items-center justify-center">
+                  <BorderBeam
+                    size={80}
+                    duration={12 * thumbnail.id}
+                    delay={9 * thumbnail.id}
+                    colorFrom="#000000"
+                    colorTo="#57AD5B"
+                  />
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <Link
+                        to={thumbnail.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <div className="relative font-chivo">
+                          <div className="flex flex-col">
+                            <CardTitle className="text-xl md:text-2xl lg:text-3xl text-[#57AD5B]">
+                              {thumbnail.title}
+                            </CardTitle>
+                            <CardDescription className="text-muted-foreground text-sm md:text-base lg:text-lg">
+                              {thumbnail.desc}
+                            </CardDescription>
+                            <div className="flex gap-2">
+                              {Object.entries(thumbnail.tools).map(
+                                ([toolName, toolIcon]) => (
+                                  <img
+                                    key={toolName}
+                                    src={toolIcon}
+                                    alt={`${toolName} icon`}
+                                    className="w-6 h-6"
+                                  />
+                                )
+                              )}
+                            </div>
+                          </div>
+                        </div>
+                      </Link>
+                    </TooltipTrigger>
+                    {createPortal(
+                      <TooltipContent className="z-[9999]">
+                        <img
+                          src={thumbnail.thumbnail}
+                          alt="Thumbnail"
+                          className="w-[500px] h-[350px] rounded shadow-lg"
+                        />
+                      </TooltipContent>,
+                      document.body
+                    )}
+                  </Tooltip>
+                </Card>
+              </TooltipProvider>
+            </BlurFade>
+          ))}
       </div>
     </>
   );

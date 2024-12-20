c84ab6b8cbdfb2949147d713c6d51a4c5035fb05
Dz.dev <darwinbjordan@gmail.com>
2024-12-20 20:09:10 +0800

    chore: adjust height in laptop

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -41,23 +41,23 @@ export default function Home() {
       <div className="relative flex items-start justify-center bg-[#ECE8E1]">
         <div className="relative mt-10">
           <div className="absolute top-[-12px] left-[-5px] w-20 h-6 bg-[#57AD5B] rotate-[-30deg] shadow-md z-10"></div>
-          <div className="absolute bottom-[-8px] right-[-6px] w-20 h-6 bg-[#57AD5B] z-10 rotate-[-60deg] shadow-md"></div>
+          <div className="absolute bottom-[-8px] right-[-6px] w-20 h-6 bg-[#57AD5B] rotate-[-60deg] shadow-md z-10"></div>
           <div className="flex flex-col sm:flex-row">
             <img
               src={images[currentImageIndex]}
-              className={`xl:max-h-[700px] lg:max-h-[600px] md:max-h-[450px] sm:max-h-[400px] max-sm:max-h-[400px] rounded-xl border-4 border-[#ECE8E1] shadow-lg transform rotate-3 transition-opacity duration-500 ${fadeCurrent ? "opacity-100" : "opacity-0"}`}
+              className={`xl:max-h-[700px] lg:max-h-[500px] md:max-h-[450px] sm:max-h-[400px] max-sm:max-h-[400px] rounded-xl border-4 border-[#ECE8E1] shadow-lg transform rotate-3 transition-opacity duration-500${fadeCurrent ? "opacity-100" : "opacity-0"}`}
               alt="Profile background"
             />
             <img
               src={images[nextImageIndex]}
-              className={`xl:max-h-[700px] lg:max-h-[600px] md:max-h-[450px] sm:max-h-[400px] max-sm:hidden rounded-xl border-4 border-[#ECE8E1] shadow-lg transform rotate-3 transition-opacity duration-500 ${fadeNext ? "opacity-100" : "opacity-0"}`}
+              className={`xl:max-h-[700px] lg:max-h-[500px] md:max-h-[450px] sm:max-h-[400px] max-sm:hidden rounded-xl border-4 border-[#ECE8E1] shadow-lg transform rotate-3 transition-opacity duration-500 ${fadeNext ? "opacity-100" : "opacity-0"}`}
               alt="Profile background"
             />
           </div>
           <div className="absolute md:bottom-8 bottom-2 right-6 text-center left-10">
             <p className="font-chivo text-white font-semibold">
               <TypingAnimation
-                className="text-md lg:text-3xl md:text-lg max-sm:text-sm"
+                className="text-md lg:text-2xl md:text-lg max-sm:text-sm"
                 text="I'm dz.dev Discover projects, skills, and new ideas as I share my
               journey as a passionate Software Engineer. Let's build
               something amazing together!"
